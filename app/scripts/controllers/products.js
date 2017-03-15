e53d90c642fae33a7d7acb65c69c829d48850bb6
msajnog <sajnog.mateusz@pollub.edu.pl>
2017-03-16 00:25:29 +0100

    fix price validation

--- app/scripts/controllers/products.js
+++ app/scripts/controllers/products.js
@@ -11,7 +11,7 @@ angular.module('supermiodek')
 .controller('ProductsCtrl', ['$scope', 'productService', 'FileUploader', 'MEDIA', function ($scope, productService, FileUploader, MEDIA) {
     $scope.media_path = MEDIA.url;
     $scope.product = {};
-    $scope.regex = '\\d+';
+    $scope.regex = '^\d+,\d+$';
 
     productService.get(function(response) {
         if (response.data) {
